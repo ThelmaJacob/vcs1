e738da15b0ef2ac690065aadb8a74b94d21b60bd
ThelmaJacob <thelmajacob.tj@gmail.com>
2026-08-26 12:48:39 +0200

    Add ExportCsvButton to TablePage component

--- app/(app)/table/page.tsx
+++ app/(app)/table/page.tsx
@@ -1,10 +1,19 @@
 import BackButton from "@/components/BackButton";
+import ExportCsvButton from "@/components/ExportCsvButton";
 import ListShell from "@/components/ListShell";
 import TableView from "@/components/TableView";
 
 export default function TablePage() {
   return (
-    <ListShell title="Table View" leading={<BackButton />}>
+    <ListShell
+      title="Table View"
+      leading={
+        <>
+          <BackButton />
+          <ExportCsvButton />
+        </>
+      }
+    >
       <TableView />
     </ListShell>
   );
