b08bc2b58069978ef432a8c4f6ac97dd006262b7
ThelmaJacob <thelmajacob.tj@gmail.com>
2026-08-26 12:09:39 +0200

    Wrap TableView in a div and add BackButton

--- app/(app)/table/page.tsx
+++ app/(app)/table/page.tsx
@@ -4,8 +4,11 @@ import TableView from "@/components/TableView";
 
 export default function TablePage() {
   return (
-    <ListShell title="Table View">
-      <TableView />
-    </ListShell>
+    <div className="space-y-4">
+      <BackButton />
+      <ListShell title="Table View">
+        <TableView />
+      </ListShell>
+    </div>
   );
 }
