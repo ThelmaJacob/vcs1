9179b36ba0426f0060dbb123da864b69b775ea9d
ThelmaJacob <thelmajacob.tj@gmail.com>
2026-08-26 12:13:56 +0200

    try

--- components/BackButton.tsx
+++ components/BackButton.tsx
@@ -1,7 +1,7 @@
-'use client' 
+'use client'
+
 import { useRouter } from 'next/navigation'
 import { ArrowLeft } from 'lucide-react'
-
 
 export default function BackButton() {
   const router = useRouter()
